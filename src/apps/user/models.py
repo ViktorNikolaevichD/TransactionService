6847c55a0a3d322dd9fa5537596309c982b131bf
VictorD <victor.zip@yandex.ru>
2025-08-06 23:20:38 +0500

    Добавить уникальность у поля email

--- src/apps/user/models.py
+++ src/apps/user/models.py
@@ -8,7 +8,7 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    email: Mapped[str] = mapped_column(Text, nullable=False)
+    email: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
     password: Mapped[str] = mapped_column(Text, nullable=False)
     full_name: Mapped[str] = mapped_column(Text, nullable=False)
     is_admin: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
