788154fbe9504199663967dbcf3b48d92022c01c
VictorD <victor.zip@yandex.ru>
2025-08-06 21:27:01 +0500

    Добавить alembic для отслеживания миграций

---
+++ src/config.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parent
+ENV_PATH = os.path.join(BASE_DIR, "envs", ".env.dev")
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    JWT_SECRET_KEY: str
+    JWT_ALG: str
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    model_config = SettingsConfigDict(env_file=ENV_PATH)
+
+
+settings = Settings()  # type: ignore
