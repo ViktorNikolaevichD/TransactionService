07aff6546e7f08e1d245b033659f6555cb70f058
VictorD <victor.zip@yandex.ru>
2025-08-07 15:57:02 +0500

    Добавить возможность получение транзакций пользователя

--- src/apps/accounting/router.py
+++ src/apps/accounting/router.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
-from apps.accounting.services import get_account_service
-from apps.accounting.schemas import SAccountRead
+from apps.accounting.services import get_account_service, get_transaction_service
+from apps.accounting.schemas import SAccountRead, STransactionRead
 from apps.user.models import User
 from apps.user.utils import get_current_admin, get_current_user
 
@@ -19,3 +19,13 @@ async def user_account(user_id: int, admin: User = Depends(get_current_admin)):
 @router.get("/all", response_model=list[SAccountRead])
 async def user_accounts(user: User = Depends(get_current_user)):
     return await get_account_service().get_accounts(user_id=user.id)
+
+
+@router.get("/{account_id}", response_model=list[STransactionRead])
+async def get_account_transactions(account_id: int, user: User = Depends(get_current_user)):
+    if user.is_admin:
+        return await get_transaction_service().get_transactions(account_id=account_id)
+    account = await get_account_service().get_account(id=account_id, user_id=user.id)
+    if not account:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return await get_transaction_service().get_transactions(account_id=account_id)
--- src/apps/accounting/services.py
+++ src/apps/accounting/services.py
@@ -1,14 +1,27 @@
+from typing import Optional
 from sqlalchemy import Sequence
 
-from apps.accounting.models import Account
-from apps.accounting.repositories import AccountRepository
+from apps.accounting.models import Account, Transaction
+from apps.accounting.repositories import AccountRepository, TransactionRepository
 from settings.services import BaseService
 
 
 class AccountService(BaseService):
+    async def get_account(self, **filter_by) -> Optional[Account]:
+        return await self.repository.find_one(**filter_by)
+    
     async def get_accounts(self, **filter_by) -> Sequence[Account]:
+        return await self.repository.find_all(**filter_by)
+
+
+class TransactionService(BaseService):
+    async def get_transactions(self, **filter_by) -> Sequence[Transaction]:
         return await self.repository.find_all(**filter_by)
 
 
 def get_account_service() -> AccountService:
     return AccountService(repo=AccountRepository)
+
+
+def get_transaction_service() -> TransactionService:
+    return TransactionService(repo=TransactionRepository)
